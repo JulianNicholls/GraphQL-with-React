f2d96a9558f2db3cbfc3c6b7837bda17b7192b7f
Julian Nicholls <juliannicholls29@gmail.com>
2019-09-25 20:53:03 +0100

    Set up for mLab, and latest mongoose

--- lyrical/index.js
+++ lyrical/index.js
@@ -1,5 +1,5 @@
 const app = require('./server/server');
 
 app.listen(4000, () => {
-  console.log('Listening');
+  console.log('Listening on Port 4000');
 });
--- lyrical/server/server.js
+++ lyrical/server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const models = require('./models');
+require('./models');
 const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -8,22 +8,26 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI =
+  'mongodb://julian:Lyrical2019@ds135830.mlab.com:35830/graphql-lyrical';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+  .once('open', () => console.log('Connected to MongoLab instance.'))
+  .on('error', error => console.log('Error connecting to MongoLab:', error));
 
 app.use(bodyParser.json());
-app.use('/graphql', expressGraphQL({
-  schema,
-  graphiql: true
-}));
+app.use(
+  '/graphql',
+  expressGraphQL({
+    schema,
+    graphiql: true,
+  })
+);
 
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
