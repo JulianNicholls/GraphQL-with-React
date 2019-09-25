42cb071af5fbb250f12487208d5ae36a6bbc0ec8
Julian Nicholls <juliannicholls29@gmail.com>
2019-09-25 11:09:28 +0100

    Write and use User schema

--- users/server.js
+++ users/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
 
+const schema = require('./schema/schema');
+
 const app = express();
 
 app.use(
   '/graphql',
   expressGraphQL({
+    schema,
     graphiql: true,
   })
 );
