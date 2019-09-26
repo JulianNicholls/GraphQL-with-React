a99a13e570fe3511e5a63b0491dcba50823c27fc
Julian Nicholls <juliannicholls29@gmail.com>
2019-09-26 21:44:22 +0100

    Update configuration for latest Webpack/Babel

--- lyrical/webpack.config.js
+++ lyrical/webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './client/index.js',
   output: {
     path: '/',
